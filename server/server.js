b5e6d2bc9539f4fb1aa95f6046fb6bd18a41b851
SabaAhmad04 <sabaahmad935@gmail.com>
2026-06-14 19:02:18 +0530

    Second Commit

--- server/server.js
+++ server/server.js
@@ -9,11 +9,14 @@ app.use(cors());
 app.use(express.json());
 
 const analyzeRoute = require("./routes/analyzeRoute");
-const PORT = process.env.PORT || 5000;
 
-app.use(express.static(
-  path.join(__dirname, "../client/dist")
-));
+app.use("/api", analyzeRoute);
+
+app.use(
+  express.static(
+    path.join(__dirname, "../client/dist")
+  )
+);
 
 app.get("*", (req, res) => {
   res.sendFile(
@@ -21,7 +24,7 @@ app.get("*", (req, res) => {
   );
 });
 
-app.use("/api", analyzeRoute);
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
